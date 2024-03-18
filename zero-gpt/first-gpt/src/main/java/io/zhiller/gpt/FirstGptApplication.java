eaa53573c9562a1e132f777aa7a149bcbeb31996
zhiyiyi <1187303661@qq.com>
2024-03-18 22:14:40 +0800

    asd

---
+++ zero-gpt/first-gpt/src/main/java/io/zhiller/gpt/FirstGptApplication.java
@@ -0,0 +1,13 @@
+package io.zhiller.gpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstGptApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FirstGptApplication.class, args);
+  }
+
+}
---
+++ zero-gpt/first-gpt/src/test/java/io/zhiller/gpt/FirstGptApplicationTests.java
@@ -0,0 +1,13 @@
+package io.zhiller.gpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstGptApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
