5408780178d8e0e87ea72707c74e31098fe0069d
zhiyiyi <1187303661@qq.com>
2024-03-19 13:28:12 +0800

    new push

--- zero-gpt/first-gpt/src/main/java/io/zhiller/gpt/FirstGptApplication.java
+++ zero-gpt/first-gpt/src/main/java/io/zhiller/gpt/FirstGptApplication.java
@@ -10,28 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class FirstGptApplication {
-
-  private Logger logger = LoggerFactory.getLogger(FirstGptApplication.class);
 
   public static void main(String[] args) {
     SpringApplication.run(FirstGptApplication.class, args);
-  }
-
-  @GetMapping("/verify")
-  public ResponseEntity<String> verify(String token) {
-    logger.debug("验证 token：{}", token);
-    if ("success".equals(token)) {
-      return ResponseEntity.status(HttpStatus.OK).build();
-    } else {
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-    }
-  }
-
-  @GetMapping("/success")
-  public String success() {
-    return "test success by xfg";
   }
 
 }
