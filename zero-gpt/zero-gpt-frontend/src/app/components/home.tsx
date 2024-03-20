fc10ba96739cacf2a4c77c575ae2a3466c643c97
zhiyiyi <1187303661@qq.com>
2024-03-20 16:18:00 +0800

    fe

--- zero-gpt/zero-gpt-frontend/src/app/components/home.tsx
+++ zero-gpt/zero-gpt-frontend/src/app/components/home.tsx
@@ -11,14 +11,16 @@ import {
   Route,
 } from "react-router-dom";
 import {Path} from "@/app/constants";
+import {useAppConfig} from "@/app/store/config";
 
 
 const Chat = dynamic(async () => (await import("./chat/chat")).Chat);
 const Role = dynamic(async () => (await import("./role/role")).Role);
 
 function Screen() {
+  const config = useAppConfig();
   return (
-    <div className={styles.container}>
+    <div className={`${config.tightBorder ? styles["tight-container"] : styles.container}`}>
       {/* 工具菜单 */}
       <SideBar/>
 
